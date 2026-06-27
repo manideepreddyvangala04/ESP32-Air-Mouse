37c26faafd19c1d8d710bee4eeae339e29118e98
Manideep Reddy Vangala <reddymanideep637@gmail.com>
2026-06-28 05:23:04 +0530

    Create config.h

---
+++ config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// PIN DEFINITIONS
+const int btnLeft         = 12; 
+const int btnRight        = 14; 
+const int btnScrollMode   = 27;
+
+// TUNING DIALS
+const float filterAlpha       = 0.25;
+const float baseSensitivity   = 16.0;
+const float accelMultiplier   = 4.0;
+const float scrollSensitivity = 0.5;
+
+#endif
